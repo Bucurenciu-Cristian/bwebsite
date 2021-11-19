2882b9251239f0060646d26f9a3cad3367645b33
cristian-bucurenciu <cristian.bucurenciu@sepa.at>
2021-11-19 12:19:09 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,347 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <!-- fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+
+  <!-- css -->
+  <link rel="stylesheet" href="css/nav.css">
+  <link rel="stylesheet" href="css/index.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/carousel.css">
+  <link rel="stylesheet" href="css/footer.css">
+
+  <title>Hello, world!</title>
+</head>
+
+<body>
+  <?php include('mainNavbar.html'); ?>
+
+
+
+  <div class="container-fluid">
+    <div class="row">
+      <!-- for desktop -->
+      <div class="container-sliced 	d-none d-sm-block">
+        <div class="bg"></div>
+        <div class="container-fluid container-withMargins">
+          <div class="row text-white padding-row">
+            <div class="col-4">
+              <span class="headline text-white">Discută cu </br> business mentorii acreditați</span>
+              <p>Participă la programele de mentoring de unu la unu cu cei mai bine pregătiți mentori din tară. Participă la programele de mentoring de unu la unu cu cei mai bine pregătiți mentori din tară. Participă la programele de mentoring de unu la unu cu cei mai bine pregătiți mentori din tară.</p>
+              <button class="btn big-button btn-black">Cauta mentor</button>
+              <div class="semiTitle pt-3"> CU O COMUNITATE DE 50+ BUSINESS MENTORI, LA UN CLICK DISTANTA POTI SA:
+              </div>
+              <ul>
+                <li>
+                  - Inveti cum sa iei deciziile cele mai bune
+                </li>
+                <li>
+                  - Inveti cum sa construiesti un buget de venituri si cheltuieli
+                </li>
+                <li>
+                  - Descoperi secrete pentru cresterea si optimizarea business-ului tau
+                </li>
+                <li>
+                  - Estimezi cat ar putea valora afacerea ta la un potential EXIT
+                </li>
+            </div>
+            <div class="col-4 image-slider">
+              <img src="assets/onetiu-dreamteam.png">
+            </div>
+            <div class="col-4">
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <!-- for mobile -->
+      <div class="container-sliced bg-pink d-block d-sm-none">
+        <div class="container-fluid container-withMargins">
+          <div class="row text-white pt-4">
+            <div class="col-12 text-center">
+              <span class="headline text-white">Discută cu business </br> mentorii acreditați</span>
+              <p>Participă la programele de mentoring de unu la unu cu cei mai bine pregătiți mentori din tară. Participă la programele de mentoring de unu la unu cu cei mai bine pregătiți mentori din tară. Participă la programele de mentoring de unu la unu cu cei mai bine pregătiți mentori din tară.</p>
+              <button class="btn big-button btn-black">Cauta mentor</button>
+
+            </div>
+            <div class="col-12">
+              <div class="semiTitle pt-3"> CU O COMUNITATE DE 50+ BUSINESS MENTORI, LA UN CLICK DISTANTA POTI SA:
+              </div>
+              <ul>
+                <li>
+                  - Inveti cum sa iei deciziile cele mai bune
+                </li>
+                <li>
+                  - Inveti cum sa construiesti un buget de venituri si cheltuieli
+                </li>
+                <li>
+                  - Descoperi secrete pentru cresterea si optimizarea business-ului tau
+                </li>
+                <li>
+                  - Estimezi cat ar putea valora afacerea ta la un potential EXIT
+                </li>
+
+            </div>
+            <div class="col-12  image-slider">
+              <img class="mx-auto d-block" src="assets/onetiu-dreamteam.png">
+            </div>
+
+          </div>
+          <div class="bg-mobile"></div>
+
+        </div>
+      </div>
+
+
+      <div class="container container-space">
+        <div class="row text-center">
+          <span class="title title-padding text-pink">"Prin toate actiunile mele, sustin activ
+            antreprenoriatul in Romania."</span>
+        </div>
+        <div class="row text-center quote">
+          <p>"Am ales sa merg inainte cu o echipa de Business Mentors de succes, asumati si dedicati cresterii unei comunitati
+            antreprenoriale puternice, aici, in Romania. Ei si-au luat aceasta raspundere, de a sustine alti antreprenori sa isi dezvolte
+            afacerile si, impreuna, sa construim un mediu antreprenorial etic, solid, durabil.
+            "</p>
+          <div class="video"><iframe src="https://www.youtube.com/embed/R9AHAqu_bis" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+        </div>
+      </div>
+
+      <div class="container-fluid container-space">
+        <div class="row justify-content-center">
+          <span class="title title-padding text-center text-pink">E mai simplu decât crezi</span>
+        </div>
+        <div class="row justify-content-center">
+          <p>Mentorul potrivit pentru tine la un click distanta</p>
+        </div>
+        <div class="row laptop">
+          <div class="col-sm-12 col-md-6">
+            <img src="assets/laptop.png" alt="laptopImage">
+          </div>
+          <div class="col-sm-12 col-md-6">
+            <div class="tickList container">
+              <div class="row">
+                <div class="col-md-1 col-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40">
+                    <g id="Group_158" data-name="Group 158" transform="translate(-940 -3090)">
+                      <path id="Path_233" data-name="Path 233" d="M20,0A20,20,0,1,1,0,20,20,20,0,0,1,20,0Z" transform="translate(940 3090)" fill="#f6284f" />
+                      <path id="Checkbox" d="M9.279,16.138,0,6.859,1.883,4.976l7.4,7.262L21.518,0,23.4,1.883Z" transform="translate(948 3101.931)" fill="#fff" />
+                    </g>
+                  </svg>
+                </div>
+                <div class="col-md-11 col-10">
+                  <p class="sub-header">Gasesti rapid si usor mentorul potrivit pentru tine</p>
+                  <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-1 col-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40">
+                    <g id="Group_158" data-name="Group 158" transform="translate(-940 -3090)">
+                      <path id="Path_233" data-name="Path 233" d="M20,0A20,20,0,1,1,0,20,20,20,0,0,1,20,0Z" transform="translate(940 3090)" fill="#f6284f" />
+                      <path id="Checkbox" d="M9.279,16.138,0,6.859,1.883,4.976l7.4,7.262L21.518,0,23.4,1.883Z" transform="translate(948 3101.931)" fill="#fff" />
+                    </g>
+                  </svg>
+                </div>
+                <div class="col-md-11 col-10 tick">
+                  <p class="sub-header">Alegi ce program ti se potriveste</p>
+                  <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-1 col-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40">
+                    <g id="Group_158" data-name="Group 158" transform="translate(-940 -3090)">
+                      <path id="Path_233" data-name="Path 233" d="M20,0A20,20,0,1,1,0,20,20,20,0,0,1,20,0Z" transform="translate(940 3090)" fill="#f6284f" />
+                      <path id="Checkbox" d="M9.279,16.138,0,6.859,1.883,4.976l7.4,7.262L21.518,0,23.4,1.883Z" transform="translate(948 3101.931)" fill="#fff" />
+                    </g>
+                  </svg>
+                </div>
+                <div class="col-md-11 col-10 tick">
+                  <p class="sub-header">Poti sa lucrezi de pe plaja</p>
+                  <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-1 col-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40">
+                    <g id="Group_158" data-name="Group 158" transform="translate(-940 -3090)">
+                      <path id="Path_233" data-name="Path 233" d="M20,0A20,20,0,1,1,0,20,20,20,0,0,1,20,0Z" transform="translate(940 3090)" fill="#f6284f" />
+                      <path id="Checkbox" d="M9.279,16.138,0,6.859,1.883,4.976l7.4,7.262L21.518,0,23.4,1.883Z" transform="translate(948 3101.931)" fill="#fff" />
+                    </g>
+                  </svg>
+                </div>
+                <div class="col-md-11 col-10 tick">
+                  <p class="sub-header">Ai totul in platforma care e usor de utilizat</p>
+                  <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <button type="button" class="btn btn-black">AFLĂ CUM FUNCȚIONEAZĂ ></button>
+        </div>
+      </div>
+
+      <div class="container-fluid carousel-desktop container-space">
+        <div class="container">
+          <div class="row title-row">
+            <span class="title title-padding text-pink">Ce spun cei care au avut sesiuni 1 la 1 cu mentorii lor</span>
+          </div>
+          <div class="row justify-content-center">
+            <p>Mentorul potrivit pentru tine la un click distanta</p>
+          </div>
+        </div>
+        <div id="demo" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="row carousel-caption">
+                <div class="col-md-6 left-column">
+                  <span class="title">Mesaj puternic care vrem sa iasa in evidenta</span>
+                  <div class="space"></div>
+                  <div class="carousel-group">
+                    <span>Programul de mentorat pe care l-am parcurs:</span>
+                    <span class="smallTitle">DECIDE IT</span>
+                  </div>
+                  <div class="carousel-group">
+                    <span>Mentorul cu care am lucrat:</span>
+                    <span class="smallTitle">Remus Bălan</span>
+                  </div>
+                </div>
+                <div class="col-md-6 right-column">
+                  <div class="row no-gutters">
+                    <div class="col-md-8">
+                      <span class="sub-header">Cristi</span>
+                    </div>
+                    <div class="col-md-4"> <img src="assets/stars.png">
+                    </div>
+                    <p class="reviewText">Nume firma SRL</p>
+                  </div>
+                  <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet, orci nec ultrices gravida, eros nisl volutpat felis, at mollis neque nibh ac lacus. Sed metus tortor, rhoncus eu nisl vel, iaculis faucibus justo. Praesent nec consequat nunc. Sed metus tortor, rhoncus eu nisl vel, iaculis faucibus justo. Praesent nec consequat nunc. Sed metus tortor, rhoncus eu nisl vel, iaculis faucibus justo. Praesent nec consequat nunc. Sed metus tortor, rhoncus eu nisl vel, iaculis faucibus justo. Praesent nec consequat nunc. Sed metus tortor, rhoncus eu nisl vel, iaculis faucibus justo. Praesent nec consequat nunc.</span>
+                </div>
+                <img src="assets/image-in-homepage-carousel.png" class="img-fluid">
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="row carousel-caption">
+                <div class="col-md-6 left-column">
+                  <span class="title">Mesaj puternic care vrem sa iasa in evidenta</span>
+                  <div class="space"></div>
+                  <div class="carousel-group">
+                    <span>Programul de mentorat pe care l-am parcurs:</span>
+                    <span class="smallTitle">DECIDE IT</span>
+                  </div>
+                  <div class="carousel-group">
+                    <span>Mentorul cu care am lucrat:</span>
+                    <span class="smallTitle">Remus Bălan</span>
+                  </div>
+                </div>
+                <div class="col-md-6 right-column">
+                  <div class="row no-gutters">
+                    <div class="col-md-8">
+                      <span class="sub-header">Cristi</span>
+                    </div>
+                    <div class="col-md-4"> <img src="assets/stars.png">
+                    </div>
+                    <p class="reviewText">Nume firma SRL</p>
+                  </div>
+                  <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet, orci nec ultrices gravida, eros nisl volutpat felis, at mollis neque nibh ac lacus. Sed metus tortor, rhoncus eu nisl vel, iaculis faucibus justo. Praesent nec consequat nunc. Sed metus tortor, rhoncus eu nisl vel, iaculis faucibus justo. Praesent nec consequat nunc. Sed metus tortor, rhoncus eu nisl vel, iaculis faucibus justo. Praesent nec consequat nunc. Sed metus tortor, rhoncus eu nisl vel, iaculis faucibus justo. Praesent nec consequat nunc. Sed metus tortor, rhoncus eu nisl vel, iaculis faucibus justo. Praesent nec consequat nunc.</span>
+                </div>
+                <img src="assets/image-in-homepage-carousel.png" class="img-fluid">
+              </div>
+            </div>
+
+          </div>
+          <a class="carousel-control-prev" href="#demo" data-slide="prev">
+            <img src="assets/left-arrow.png">
+          </a>
+          <a class="carousel-control-next" href="#demo" data-slide="next">
+            <img src="assets/right-arrow.png">
+          </a>
+        </div>
+        <div class="row justify-content-center">
+          <button class="btn normal-button mt-5 btn-black">VREAU ȘI EU SA IMI CRESC AFACEREA RAPID ></button>
+        </div>
+      </div>
+
+
+
+      <div class="container-fluid carousel-mobile container-withMargins container-space">
+        <div class="container">
+          <div class="row text-center">
+            <span class="title text-pink">Ce spun cei care au avut sesiuni 1 la 1 cu mentorii lor</span>
+          </div>
+          <div class="row pt-4 justify-content-center">
+            <p>Mentorul potrivit pentru tine la un click distanta</p>
+          </div>
+        </div>
+        <div id="demo" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="row carousel-caption">
+                <div class="col-12">
+                  <span class="title">Mesaj puternic care vrem sa iasa in evidenta</span>
+                  <div class="space"></div>
+                  <div class="carousel-group">
+                    <span>Programul de mentorat pe care l-am parcurs:</span>
+                    <span class="smallTitle">DECIDE IT</span>
+                  </div>
+                  <div class="carousel-group">
+                    <span>Mentorul cu care am lucrat:</span>
+                    <span class="smallTitle">Remus Bălan</span>
+                  </div>
+                  <div class="carousel-mobile-photo text-center mt-5">
+                    <img src="assets/image-in-homepage-carousel.png">
+                  </div>
+                </div>
+                <div class="col-12 text-grey pt-5">
+                  <div class="row no-gutters">
+                    <div class="col-4">
+                      <span class="sub-header">Cristi</span>
+                    </div>
+                    <div class="col-8 text-right"> <img src="assets/stars.png">
+                    </div>
+                    <p class="reviewText">Nume firma SRL</p>
+                  </div>
+                  <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet, orci nec ultrices gravida, eros nisl volutpat felis, at mollis neque nibh ac lacus. Sed metus tortor, rhoncus eu nisl vel, iaculis faucibus justo. Praesent nec consequat nunc. Sed metus tortor, rhoncus eu nisl vel, iaculis faucibus justo. Praesent nec consequat nunc. Sed metus tortor, rhoncus eu nisl vel, iaculis faucibus justo. Praesent nec consequat nunc. Sed metus tortor, rhoncus eu nisl vel, iaculis faucibus justo. Praesent nec consequat nunc. Sed metus tortor, rhoncus eu nisl vel, iaculis faucibus justo. Praesent nec consequat nunc.</span>
+                </div>
+
+              </div>
+            </div>
+
+
+          </div>
+          <a class="carousel-control carousel-control-prev" href="#demo" data-slide="prev">
+            <img src="assets/left-arrow.png">
+          </a>
+          <a class="carousel-control carousel-control-next" href="#demo" data-slide="next">
+            <img src="assets/right-arrow.png">
+          </a>
+        </div>
+        <div class="container-withMargins row justify-content-center">
+          <button class="btn normal-button mt-5 btn-black">VREAU ȘI EU SA IMI CRESC AFACEREA RAPID ></button>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+  <!-- jQuery,Popper.js, Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+<?php include('mainFooter.html'); ?>
+
+</html>
